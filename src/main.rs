3a6c1697c6e948c4a06eed0f9bde89f9c5a13e44
Erik Mossberg <ClicketyClack@users.noreply.github.com>
2016-10-23 23:57:40 +0200

    Add extern crate portaudio + simple version_text() call to verify.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 extern crate piston_window;
+extern crate portaudio;
 
 use piston_window::*;
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Hello World!", [512; 2]).build().unwrap();
+
+    let mut pa = portaudio::PortAudio::new().unwrap();
+    let title = format!("Portaudio version {:?}", pa.version_text());
+    let mut window: PistonWindow = WindowSettings::new(title, [512; 2]).build().unwrap();
+
+
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
             clear([0.5, 0.5, 0.5, 1.0], g);
