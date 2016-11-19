0a86a464694eae41a5f8585097deaa35e27a3235
Erik Mossberg <clicketyclack@users.noreply.github.com>
2016-11-19 21:54:23 +0100

    Move IOSettings to seperate audiodev_config module.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+/*! 
+Hertzery. A mod.
+
+*/
+
+
 extern crate piston_window;
 extern crate sdl2_window;
 extern crate portaudio;
 extern crate time;
 
-
+pub mod dsp_waveforms;
+pub mod audiodev_config;
 
 fn main() {
     hertzery::main();
@@ -15,98 +22,15 @@ mod hertzery {
     use portaudio;
     use portaudio as pa;
     use time;
+    use audiodev_config::*;
     
     use piston_window::{PistonWindow, WindowSettings, OpenGL};
     use sdl2_window::Sdl2Window;
 
+    
     pub fn main() {
         run().unwrap()
     }
-
-    struct ExtractedInfo<'a> {
-        def_input: portaudio::DeviceIndex,
-        def_output: portaudio::DeviceIndex,
-        input_latency: f64,
-        output_latency: f64,
-        pa: &'a portaudio::PortAudio,
-    }
-
-    impl<'a> ExtractedInfo<'a> {
-        fn new(pa: &portaudio::PortAudio) -> ExtractedInfo {
-            let def_input = pa.default_input_device().unwrap();
-            let def_output = pa.default_output_device().unwrap();
-
-            let input_info = pa.device_info(def_input).unwrap();
-            let output_info = pa.device_info(def_output).unwrap();
-
-            let input_latency = input_info.default_low_input_latency;
-            let output_latency = output_info.default_low_output_latency;
-
-            ExtractedInfo {
-                def_input: def_input,
-                def_output: def_output,
-                input_latency: input_latency,
-                output_latency: output_latency,
-                pa: &pa,
-            }
-        }
-    }
-
-
-    struct IOSettings<'a> {
-        sample_rate: f64,
-        pa_settings: portaudio::stream::DuplexSettings<f32, f32>,
-        pa_handle: &'a portaudio::PortAudio,
-        chunk_size: u32,
-        is_interleaved: bool,
-        input_device : portaudio::DeviceIndex,
-        output_device : portaudio::DeviceIndex,
-    }
-
-    impl<'a> IOSettings<'a> {
-        fn quickstart(pa: &portaudio::PortAudio) -> IOSettings {
-
-            IOSettings::print_devices(pa);
-
-            let sample_rate = 48000.0;
-            let chunk_size = 2048; // Gives ~ 43ms chunks
-            let is_interleaved = true;
-            let einfo = ExtractedInfo::new(&pa);
-            let input_device = einfo.def_input;
-            let output_device = einfo.def_output;
-
-            let input_params =
-                pa::StreamParameters::<f32>::new(input_device, 1, is_interleaved, einfo.input_latency);
-            let output_params =
-                pa::StreamParameters::new(output_device, 2, is_interleaved, einfo.output_latency);
-                
-            let settings =
-                pa::DuplexStreamSettings::new(input_params, output_params, sample_rate, chunk_size);
-
-            IOSettings {
-                sample_rate: sample_rate,
-                pa_settings: settings,
-                pa_handle: &pa,
-                chunk_size : chunk_size,
-                is_interleaved : is_interleaved,
-                input_device : input_device,
-                output_device : output_device,
-            }
-        }
-        
-        fn print_devices(pa: &portaudio::PortAudio) -> () {
-            if let Ok(mut iter) = pa.devices() {
-                while let Some(d) = iter.next() {
-                    let dinfo = d.unwrap();
-                    let idx = dinfo.0;
-                    let name = dinfo.1.name;
-                    println!("Found device {:?}, {}", idx, name);
-                    
-                }
-            }
-        }
-    }
-
 
 
     struct RecordingPath<'a> {
@@ -167,6 +91,8 @@ mod hertzery {
             stream
         }
     }
+
+
 
     fn run() -> Result<(), pa::Error> {
 
