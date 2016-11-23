33d2aadcaca74853bc185d69dc677079a74bd647
Erik Mossberg <clicketyclack@users.noreply.github.com>
2016-11-23 19:05:13 +0100

    Trigger UI update / redraws.

--- src/ui.rs
+++ src/ui.rs
@@ -8,11 +8,14 @@ use program::ReportGlobals;
 pub fn start_ui<'a>(report_globals : &'a ReportGlobals) {
     
     use std::fs::OpenOptions;
+    
+    use std::{thread, time};
+
     use self::gtk::prelude::*;
     use self::gtk;
     use self::gdk;
     use self::cairo;    
-    use self::gtk::{Builder, Button, MessageDialog, Window};
+    use self::gtk::{Builder, Button, MessageDialog, Window, DrawingArea};
 
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
@@ -36,18 +39,26 @@ pub fn start_ui<'a>(report_globals : &'a ReportGlobals) {
     let rg = report_globals.current_volume.clone();
 
     drawing_area.connect_draw(move |widget, cairo_context| {
-            
-            let mut data = rg.lock().unwrap();
-            let radius = (*data).abs() as f64;
-            println!("Drawing with radius {}!", radius);
-            cairo_context.set_source_rgb(0.9,0.4,0.1);
-            cairo_context.rectangle(20.3,20.3,155.5,155.5);
-            cairo_context.fill();
+            { // Scope because we don't want to keep the lock during the sleep.
 
-            cairo_context.set_source_rgb(0.3,0.3,0.8);
-            cairo_context.arc(120.1,70.6,radius, 0.1, 10.9);
-            cairo_context.fill();
-        
+                let mut data = rg.lock().unwrap();
+                let radius = (*data).abs() as f64;
+                println!("Drawing with radius {}!", radius);
+                cairo_context.set_source_rgb(0.9,0.4,0.1);
+                cairo_context.rectangle(20.3,20.3,155.5,155.5);
+                cairo_context.fill();
+    
+                cairo_context.set_source_rgb(0.3,0.3,0.8);
+                cairo_context.arc(120.1,70.6,radius, 0.1, 10.9);
+                cairo_context.fill();
+            }
+            
+            DrawingArea::queue_draw_area(widget,0,2,345,377);
+            //gdk::Window::invalidate_rect(widget,0,0);
+            
+            // TODO: Seperate timer here.
+            let ten_millis = time::Duration::from_millis(10);
+            thread::sleep(ten_millis);
         
             Inhibit(false)
         });
